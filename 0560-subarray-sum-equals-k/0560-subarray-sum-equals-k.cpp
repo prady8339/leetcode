eff927e1e4821530ec754842dc6198c2b245a1a6
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-07-24 23:27:45 +0530

    Time: 78 ms (99.45%), Space: 41.4 MB (91.64%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,30 +3,16 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         int ct = 0;
-        int presum = 0;
+        int psum = 0;
         unordered_map<int,int> un;
-        un[0]=1;
-        for(int i = 0 ; i < n ;i++){
-            presum+=nums[i];
-            if(un.count(presum-k)){
-                ct+=un[presum-k];
+        un[0]++;
+        for(auto x:nums){
+            psum+=x;
+            if(un.count(psum-k)){
+                ct+=un[psum-k];
             }
-            un[presum]++;
+            un[psum]++;
         }
         return ct;
     }
 };
-
-
-//   unordered_map<int,int>m;
-//         int s=0;
-//         int c=0;
-//         m[0]=1;
-//         for(int i=0;i<nums.size();i++){
-//             s+=nums[i];  // calculating prefix sum [sum from 0 to i]
-//             if(m.find(s-k)!=m.end()){
-//                 c+=m[s-k];  // adding no of prefix sums encountered till now with sum s-k.
-//             }
-//             m[s]++; // adding prefix sum to the map
-//         }
-//         return c;
