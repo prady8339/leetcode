f22e6c0ea8cdd362389314546e6122e53b14eaca
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-05 00:49:50 +0530

    Time: 8 ms (31.70%) | Memory: 8.4 MB (25.69%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -6,26 +6,20 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };4
+ * };
  */
 class Solution {
-    private:
-    ListNode* solve(ListNode* root){
-
-            if (root == nullptr || root->next == nullptr) return root;
-        ListNode* curr = solve(root->next);
-        root->next->next = root;
-        root->next = nullptr;
-        return curr;
-        // if(root==NULL || root->next==NULL ) return root;
-        // ListNode* curr = solve(root->next);
-        // curr->next = root;
-        // return curr;
-    }
-
-   
 public:
     ListNode* reverseList(ListNode* head) {
-        return solve(head);
+        if(!head || !head->next) return head;
+        ListNode* a=head,*b=head->next;
+        a->next = NULL;
+        while(b!=NULL){
+            ListNode* temp = b->next;
+            b->next = a;
+            a=b;
+            b=temp;
+        }
+        return a;
     }
 };
