666523f831fc17984426e75d8ebfc04f532f234f
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-07 18:09:05 +0530

    Added solution - LeetHub

--- Minimum steps to minimize n as per given condition - GFG/minimum-steps-to-minimize-n-as-per-given-condition.cpp
+++ Minimum steps to minimize n as per given condition - GFG/minimum-steps-to-minimize-n-as-per-given-condition.cpp
@@ -4,44 +4,41 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    private:
-   private:
-int solve(int n, unordered_map<int, int> &dp, int &steps) {
-    if (n == 1) return steps;
-    if (dp.count(n)) return dp[n];
+private:
+    int solve(int n, vector<int>& dp, int& steps) {
+        if (n == 1) return steps;
+        if (dp[n] != -1) return dp[n];
 
-    int a = INT_MAX, b = INT_MAX, c = INT_MAX;
-    if (n % 3 == 0) {
+        int a = INT_MAX, b = INT_MAX, c = INT_MAX;
+        if (n % 3 == 0) {
+            steps++;
+            a = solve(n / 3, dp, steps);
+            steps--;
+        }
+        if (n % 2 == 0) {
+            steps++;
+            b = solve(n / 2, dp, steps);
+            steps--;
+        }
         steps++;
-        a = solve(n / 3, dp, steps);
+        c = solve(n - 1, dp, steps);
         steps--;
-    }
-    if (n % 2 == 0) {
-        steps++;
-        b = solve(n / 2, dp, steps);
-        steps--;
-    }
-    steps++;
-    c = solve(n - 1, dp, steps);
-    steps--;
 
-    return dp[n] = 1 + min({a, b, c});
-}
+        return dp[n] = 1 + min({a, b, c});
+    }
 
-	public:
-	int minSteps(int N) 
-	{ 
-	    if(N==1) return 0;
-	    if(N==3 || N==2) return 1;
-	    unordered_map<int,int> dp;
-	    dp[1]=0;
-	    dp[2]=1;
-	    dp[3]=1;
-	    int steps = 0;
-	    return solve(N,dp,steps);
-	} 
+public:
+    int minSteps(int N) {
+        if (N == 1) return 0;
+        if (N == 3 || N == 2) return 1;
+        vector<int> dp(N + 1, -1); // Initialize dp vector with -1
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        int steps = 0;
+        return solve(N, dp, steps);
+    }
 };
-
 
 
 //{ Driver Code Starts.
