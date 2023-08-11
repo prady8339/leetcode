30173e496967c7d04acb62876c00312eb3d8940e
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-11 15:19:02 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -7,27 +7,44 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
-class Solution{   
+class Solution{  
+    bool solve(vector<vector<int>> &dp,vector<int> &arr,int sum,int idx){
+    if(sum == 0) return true;
+    if(idx < 0) return false;
+    if(sum < 0) return false;
+       if(dp[idx][sum]!=-1) return dp[idx][sum];
+        bool pick = solve(dp,arr,sum-arr[idx],idx-1);
+        bool npick = solve(dp,arr,sum,idx-1);
+        return dp[idx][sum]=(pick)||(npick);
+    }
 public:
     bool isSubsetSum(vector<int>arr, int sum){
-        int N = arr.size();
-        vector<vector<bool>> t(N+1,vector<bool> (sum+1));
-        for(int i = 0 ; i < N+1 ; i++){
-            for(int j = 0 ; j < sum+1 ; j++){
-                if(i==0) t[i][j]=false;
-                if(j==0) t[i][j]=true;
-            }
-        }
+        vector<vector<int>> dp(arr.size()+1,vector<int> (sum+1,-1));
+        return solve(dp,arr,sum,arr.size()-1);
+        // int N = arr.size();
+        // vector<vector<bool>> t(N+1,vector<bool> (sum+1));
+        // for(int i = 0 ; i < N+1 ; i++){
+        //     for(int j = 0 ; j < sum+1 ; j++){
+        //         if(i==0) t[i][j]=false;
+        //         if(j==0) t[i][j]=true;
+        //     }
+        // }
         
-        for(int i = 1 ; i < N+1 ; i++){
-            for(int j = 1 ; j < sum+1 ; j++){
-                if(arr[i-1]<=j){
-                    t[i][j]= t[i-1][j-arr[i-1]]  || t[i-1][j];
-                    }
-                else t[i][j]=t[i-1][j];
-            }
-        }
-        return t[N][sum];
+        // for(int i = 1 ; i < N+1 ; i++){
+        //     for(int j = 1 ; j < sum+1 ; j++){
+        //         if(arr[i-1]<=j){
+        //             t[i][j]= t[i-1][j-arr[i-1]]  || t[i-1][j];
+        //             }
+        //         else t[i][j]=t[i-1][j];
+        //     }
+        // }
+        // for(int i = 0 ; i < N+1 ; i++){
+        //     for(int j = 0 ; j < sum+1 ; j++){
+        //         cout<<t[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // return t[N][sum];
         
     }
 };
