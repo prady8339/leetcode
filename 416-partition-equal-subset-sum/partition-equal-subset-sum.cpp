76f9d9de0e0176572e8fd1c082b6b960e04853b3
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-11 19:59:04 +0530

    Time: 206 ms (81.97%) | Memory: 11.3 MB (86.12%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -44,26 +44,44 @@ public:
 
     // top down dp
 
-    vector<vector<int>> dp(n+1,vector<int> (target+1));
+    // vector<vector<int>> dp(n+1,vector<int> (target+1));
+    // for(int j = 0 ; j < target+1 ; j++){
+    //     dp[0][j]=0;
+    // }
+    //  for(int i = 0 ; i < n+1 ; i++){
+    //     dp[i][0]=1;
+    // }
 
-   
-    for(int j = 0 ; j < target+1 ; j++){
-        dp[0][j]=0;
-    }
-     for(int i = 0 ; i < n+1 ; i++){
-        dp[i][0]=1;
-    }
+    // for(int i = 1 ; i < n+1 ; i++){
+    //     for(int j = 1 ; j < target+1 ; j++){
+    //         if(j>=nums[i-1]){
+    //             dp[i][j]=(dp[i-1][j-nums[i-1]])||(dp[i-1][j]);
+    //         }else{
+    //             dp[i][j]=dp[i-1][j];
+    //         }
+    //     }
+    // }
 
-    for(int i = 1 ; i < n+1 ; i++){
-        for(int j = 1 ; j < target+1 ; j++){
-            if(j>=nums[i-1]){
-                dp[i][j]=(dp[i-1][j-nums[i-1]])||(dp[i-1][j]);
-            }else{
-                dp[i][j]=dp[i-1][j];
-            }
+    // return dp[n][target];
+
+    // space optimized top down
+    vector<int> prev(target+1);
+    vector<int> curr(target+1);
+    prev[0] = 1;
+    curr[0] = 1;
+
+   for (int i = 1; i < n+1; i++) {
+    for (int j = 1; j< target+1 ; j++) {  
+        if (j >= nums[i-1]) {
+            curr[j] = prev[j-nums[i-1]] || prev[j];
+        } else {
+            curr[j] = prev[j];
         }
     }
+    prev = curr;
+    }
 
-    return dp[n][target];
+    return curr[target];
+    
     }
 };
