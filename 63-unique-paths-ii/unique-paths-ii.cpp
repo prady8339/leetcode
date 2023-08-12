b6b56259e7dcd7489605718cad2ed6294dc34d08
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-12 11:56:48 +0530

    Time: 5 ms (28.03%) | Memory: 7.9 MB (6.12%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,35 @@
 class Solution {
-    private:
-    int solve(vector<vector<int>>& og,vector<vector<int>>& dp,int i,int j){
-        if(i>=og.size() || j>=og[0].size()) return 0;
-        if(og[i][j]==1) return 0;
-        if(i==og.size()-1 && j==og[0].size()-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = solve(og,dp,i+1,j);
-        int right = solve(og,dp,i,j+1);
-        return dp[i][j] = down + right;
-    }
+   
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& og) {
-        int n = og.size(),m=og[0].size();
-        //if(og[0][0]==1) return 0;
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return solve(og,dp,0,0);
+         int n = og.size(), m = og[0].size();
+        vector<vector<long long>> dp(n, vector<long long>(m, 0));
+
+        // Initialize the first cell to 1 if it's not an obstacle
+        if (og[0][0] == 0)
+            dp[0][0] = 1;
+
+        // Initialize the first column
+        for (int i = 1; i < n; i++) {
+            if (og[i][0] == 0)
+                dp[i][0] = dp[i - 1][0];
+        }
+
+        // Initialize the first row
+        for (int j = 1; j < m; j++) {
+            if (og[0][j] == 0)
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        // Fill in the dp matrix using dynamic programming
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (og[i][j] == 0)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[n - 1][m - 1];
+    
     }
 };
