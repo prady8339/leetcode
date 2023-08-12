e973abeb73c103fcf6ced8a5194e96e2fd6929c2
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-12 19:05:36 +0530

    Time: 5 ms (28.03%) | Memory: 7.8 MB (25.52%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,23 +5,21 @@ public:
          int n = og.size(), m = og[0].size();
         vector<vector<int>> dp(n, vector<int>(m, 0));
 
-        // Initialize the first cell to 1 if it's not an obstacle
         if (og[0][0] == 0)
             dp[0][0] = 1;
 
-        // Initialize the first column
+
         for (int i = 1; i < n; i++) {
             if (og[i][0] == 0)
                 dp[i][0] = dp[i - 1][0];
         }
 
-        // // Initialize the first row
         for (int j = 1; j < m; j++) {
             if (og[0][j] == 0)
                 dp[0][j] = dp[0][j - 1];
         }
 
-        // Fill in the dp matrix using dynamic programming
+ 
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (og[i][j] == 0)
