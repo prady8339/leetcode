fd57014e8c1523496d1d28391f9dea235d5128b3
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-12 19:31:32 +0530

    Time: 5 ms (28.03%) | Memory: 7.8 MB (25.52%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,33 +1,30 @@
 class Solution {
-   
+
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& og) {
-         int n = og.size(), m = og[0].size();
-        vector<int> dp(m);
+int uniquePathsWithObstacles(vector<vector<int>>& og) {
+    int n = og.size(), m = og[0].size();
+    vector<int> dp(m);
 
-       // if (og[0][0] == 0)
-            dp[0] = 1;
+    dp[0] = 1;
 
-        // for (int j = 1; j < m; j++) {
-        //     if (og[0][j] == 0)
-        //         dp[j] = dp[j - 1];
-        // }
-
-        for (int i = 0; i < n; i++) {
-             vector<int> ndp = dp;
-            // if(og[i][0] == 0) dp[0]+=dp[]
-            for (int j = 0; j < m; j++) {
-                if(j>0)
-                 ndp[j]+=ndp[j-1];
-                 if(og[i][j] == 1)  
-                 ndp[j]=0;
-                 
-
+    for (int i = 0; i < n; i++) 
+    {
+        vector<int> ndp = dp;
+        for (int j = 0 ; j < m ; j++) 
+        {
+            if(j > 0 )
+            {
+                ndp[j] += ndp[j-1];
             }
-            dp=ndp;
+            if(og[i][j] == 1)
+            { 
+                ndp[j] = 0;
+            }
         }
+        dp = ndp;
+    }
 
-        return dp[m - 1];
-    
+    return dp[m - 1];
+
     }
 };
