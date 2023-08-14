e03e21742119b01022ca48ee4b0c79b07fb03428
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-14 16:41:16 +0530

    Time: 14 ms (74.00%) | Memory: 12.4 MB (38.51%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,32 @@
 class Solution {
-private:
-int solve(vector<int>& nums, int target,int idx,int exp,vector<unordered_map<int,int>> &dp){
-    if(idx>=nums.size()){
-        return exp==target?1:0;
-    }
-    if(dp[idx].count(exp)) return dp[idx][exp];
-
-    int neg = solve(nums,target,idx+1,exp-nums[idx],dp);
-    int pos = solve(nums,target,idx+1,exp+nums[idx],dp);
-
-    return dp[idx][exp] = neg+pos;
-
-}
-
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        //idx,exp
         int n = nums.size();
-        vector<unordered_map<int,int>> dp(n+1);
-        return solve(nums,target,0,0,dp);
+        if(n==0) return target==0;
+        if(n==1) return target==nums[0] || target== -1*nums[0];
+        int sum = 0;
+        for (auto x : nums) sum += x;
+
+        if(target > sum  || target < -sum) return 0;
+        
+        if((sum+target) % 2 != 0) return 0;
+
+        int req = (sum + target) / 2;
+
+        vector<vector<int>> dp(n + 1, vector<int>(req + 1));
+        dp[0][0] = 1;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < req + 1; j++) {
+                dp[i][j] = dp[i - 1][j];  // Initialize with previous value
+
+                if (j - nums[i - 1] >= 0) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]]; // Update with adding current number
+                }
+
+            }
+        }
+
+        return dp[n][req];
     }
 };
