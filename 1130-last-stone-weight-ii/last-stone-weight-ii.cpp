9d6a55311567d4b8c3d4b57e3b5ab0d046c1c381
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-14 10:56:26 +0530

    Time: 4 ms (77.76%) | Memory: 9.6 MB (35.63%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
-private:
-    int solve(vector<int> &stones,vector<vector<int>> &dp,int i,int target){
-        if(target==0) return 0;
-        if(i>=stones.size()) return 0;
 
-        if(dp[i][target]!=-1) return dp[i][target];
-        int pick = 0,npick = 0;
-        if(target-stones[i]>=0){
-            pick = stones[i]+solve(stones,dp,i+1,target-stones[i]);
-        }
-        npick = solve(stones,dp,i+1,target);
-        return dp[i][target] = max(pick,npick);
-    }
 public:
     int lastStoneWeightII(vector<int>& stones) {
         int sum = 0 , n = stones.size();
         for(auto x:stones) sum+=x;
         int target = sum/2;
-        vector<vector<int>> dp(n+1,vector<int> (target+1,-1));
-        int res = solve(stones,dp,0,target);
+        vector<vector<int>> dp(n+1,vector<int> (target+1));
+
+        for(int i = 1; i < n+1 ; i++){
+            for(int j = 1; j < target+1 ; j++){
+                if(j>=stones[i-1]){
+                    dp[i][j]=max(stones[i-1]+dp[i-1][j-stones[i-1]],dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }       
+
+        int res = dp[n][target];
         return (sum-2*res);
     }
 };
