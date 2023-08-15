ae90ffe0edb958a07efa496d4eacc624da83aa36
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-15 19:27:58 +0530

    Time: 4 ms (78.23%) | Memory: 10.3 MB (48.14%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -11,43 +11,24 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(!head || !head->next) return head;
-        ListNode *before = NULL, *after = NULL;
-        ListNode *before_head=NULL,*after_head=NULL;
+        ListNode before_head,after_head;
+        ListNode *before= &before_head, *after = &after_head;
 
-        ListNode *temp = head;
-        while (temp) {
-            if (temp->val < x) {
-                if(!before_head){
-                    before_head=temp;
-                    before=before_head;
-                }else{
-                    before->next=temp;
-                    before = before->next;
-                }
-               
-                
-            } else {
-
-                if(!after_head){
-                    after_head=temp;
-                    after=after_head;
-                }else{
-                    after->next = temp;
-                    after=after->next;
-                }  
-                
+        ListNode* temp = head;
+        while(temp){
+            if(temp->val < x){
+                before->next = temp;
+                before=before->next;
+            }else{
+                after->next = temp;
+                after=after->next;
             }
-            temp = temp->next;
+            temp=temp->next;
         }
 
-        if(after)
-        after->next = NULL;  
-        
-        if(before)
-        before->next = after_head; 
-        else return head;
+        after->next = NULL;
+        before->next = after_head.next;
 
-        return before_head;
+        return before_head.next;
     }
 };
