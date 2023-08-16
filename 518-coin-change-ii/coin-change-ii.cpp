f1dfd7f86f3c23c166af7dd2a93ba049128dd74e
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-16 15:43:57 +0530

    Time: 34 ms (35.13%) | Memory: 18.6 MB (32.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,15 +1,32 @@
 class Solution {
+private:
+    int solve(int amt,vector<int> &coins,vector<int> &dp){
+        if(amt==0) return 1;
+        int sum = 0;
+        for(auto coin:coins){
+            if(amt-coin>=0)
+            sum+=solve(amt-coin,coins,dp);
+        }
+        return sum;
+    }
+
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount + 1, 0);
-        dp[0] = 1;
-        
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] += dp[j - coin];
-            }
+    int change(int amt, vector<int>& coins) {
+       int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int> (amt+1));
+        for(int i = 0 ; i < n+1 ; i++){
+            dp[i][0]=1;
         }
         
-        return dp[amount];
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < amt+1 ; j++){
+                if(j>=coins[i-1]){
+                    dp[i][j] = dp[i][j-coins[i-1]]+dp[i-1][j];
+                }else{
+                dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amt];
     }
 };
