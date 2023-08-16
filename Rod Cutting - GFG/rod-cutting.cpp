b546db656e8737eee1831ac21c0d0eccb4b05ef3
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-16 11:53:34 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -32,10 +32,10 @@ private:
         for(int i = 1 ; i < n+1 ; i++){
             for(int j = 0 ; j < n+1; j++){
                 
+                dp[i][j]=dp[i-1][j];
+                 
                 if(j>=i){
                     dp[i][j]=max(price[i-1]+dp[i][j-i],dp[i-1][j]);
-                }else{
-                    dp[i][j]=dp[i-1][j];
                 }
                 
             }
