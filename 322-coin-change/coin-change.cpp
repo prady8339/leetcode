240297e1759a7dc6d9037a25ff6374225f513312
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-16 21:01:38 +0530

    Time: 125 ms (38.77%) | Memory: 49.3 MB (12.96%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int> &coins,int amt,vector<int> &dp){
-        if(amt==0)
-        return 0;
-        
-        if(dp[amt]!=-1)
-        return dp[amt];
-        
-        int ans=1e9;
-        for(auto it:coins){
-            if(amt>=it){
-                ans=min(ans,solve(coins,amt-it,dp)+1);
-            }
+    int coinChange(vector<int>& coins, int amt) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int> (amt+1));
+  
+        for(int j = 0 ; j < amt+1 ; j++){
+            dp[0][j]=1e5;
         }
 
-        return dp[amt] = ans;
-    }
-    int coinChange(vector<int>& coins, int amt) {
-        vector<int> dp(amt+1,-1);
-        int ans=solve(coins,amt,dp);
-        return ans==(1e9)?-1:ans;
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1; j < amt+1 ; j++){
+                if(j>=coins[i-1]){
+                    dp[i][j] = min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amt]==1e5?-1:dp[n][amt];
     }
 };
