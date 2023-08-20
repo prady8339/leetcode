9d6eadf1d99bd057fe6055b74a5248701c0e3b86
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-20 17:34:43 +0530

    Time: 104 ms (86.63%) | Memory: 20.4 MB (49.73%) - LeetSync

--- 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
+++ 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.cpp
@@ -1,88 +1,100 @@
-class UnionFind {
-private:
-    int numSets;
-    vector<int> parent;
-    vector<int> rank;
-public:
-    UnionFind(int n) {
-        numSets = n;
-        parent.resize(n);
-        rank.resize(n, 0);
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    int getSets() {
-        return numSets;
-    }
-
-    int getParent(int u) {
-        if(parent[u] != u) {
-            return getParent(parent[u]);
-        }
-        return parent[u];
-    }
-
-    void unionSets(int u, int v) {
-        int p1 = getParent(u), p2 = getParent(v);
-        if(p1 == p2) {
-            return;
-        }
-        if(rank[p1] > rank[p2]) {
-            parent[p2] = p1;
-        }
-        else if(rank[p1] < rank[p2]) {
-            parent[p1] = p2;
-        }
-        else {
-            parent[p2] = p1;
-            rank[p1]++;
-        }
-        numSets--;
-    }
-};
-
 class Solution {
-private:
-    int getMST(int n, vector<vector<int>>& treeEdges, vector<int> includedEdge, vector<int> excludedEdge) {
-        UnionFind uf(n);
-        int cost = 0;
-        if(!includedEdge.empty()) {
-            uf.unionSets(includedEdge[1], includedEdge[2]);
-            cost += includedEdge[0];
-        }
-        for(auto edge : treeEdges) {
-            if(uf.getSets() == 1) {
-                break;
-            }
-            if(edge == excludedEdge || edge == includedEdge || uf.getParent(edge[1]) == uf.getParent(edge[2])) {
-                continue;
-            }
-            uf.unionSets(edge[1], edge[2]);
-            cost += edge[0];
-        }
-        return uf.getSets() == 1 ? cost : INT_MAX;
-    }
 public:
+    class UnionFind {
+    public:
+        vector<int> parent;
+        vector<int> size;
+        int maxSize;
+
+        UnionFind(int n) {
+            parent.resize(n);
+            size.resize(n, 1);
+            maxSize = 1;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int find(int x) {
+            // Finds the root of x
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        bool unite(int x, int y) {
+            // Connects x and y
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (size[rootX] < size[rootY]) {
+                    swap(rootX, rootY);
+                }
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+                maxSize = max(maxSize, size[rootX]);
+                return true;
+            }
+            return false;
+        }
+    };
+
     vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> result(2);
-        vector<vector<int>> treeEdges;
-        int x = 0;
-        for(auto edge : edges) {
-            treeEdges.push_back({edge[2], edge[0], edge[1], x++});
+        auto newEdges = edges;
+        // Add index to edges for tracking
+        int m = newEdges.size();
+        for (int i = 0; i < m; i++) {
+            newEdges[i].push_back(i);
         }
-        sort(treeEdges.begin(), treeEdges.end());
-        int mstCost = getMST(n, treeEdges, {}, {});
-        for(auto edge : treeEdges) {
-            if(mstCost < getMST(n, treeEdges, {}, edge)) {
-                result[0].push_back(edge[3]);
-            }
-            else if(mstCost == getMST(n, treeEdges, edge, {})) {
-                result[1].push_back(edge[3]);
+
+        // Sort edges based on weight
+        sort(newEdges.begin(), newEdges.end(), [](auto& a, auto& b) {
+            return a[2] < b[2];
+        });
+
+        // Find MST weight using union-find
+        UnionFind ufStd(n);
+        int stdWeight = 0;
+        for (const auto& edge : newEdges) {
+            if (ufStd.unite(edge[0], edge[1])) {
+                stdWeight += edge[2];
             }
         }
-        
-        return result;
+
+        vector<vector<int>> results(2);
+        // Check each edge for critical and pseudo-critical
+        for (int i = 0; i < m; i++) {
+            UnionFind ufIgnore(n);
+            int ignoreWeight = 0;
+            for (int j = 0; j < m; j++) {
+                if (i != j && ufIgnore.unite(newEdges[j][0], newEdges[j][1])) {
+                    ignoreWeight += newEdges[j][2];
+                }
+            }
+
+            // If the graph is disconnected or the total weight is greater, 
+            // the edge is critical
+            if (ufIgnore.maxSize < n || ignoreWeight > stdWeight) {
+                results[0].push_back(newEdges[i][3]);
+            } else {
+                // Force this edge and calculate MST weight
+                UnionFind ufForce(n);
+                ufForce.unite(newEdges[i][0], newEdges[i][1]);
+                int forceWeight = newEdges[i][2];
+                for (int j = 0; j < m; j++) {
+                    if (i != j && ufForce.unite(newEdges[j][0], newEdges[j][1])) {
+                        forceWeight += newEdges[j][2];
+                    }
+                }
+
+                // If total weight is the same, the edge is pseudo-critical
+                if (forceWeight == stdWeight) {
+                    results[1].push_back(newEdges[i][3]);
+                }
+            }
+        }
+
+        return results;
     }
 };
