910f015c52743006385dca36912657d32c35d179
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-22 05:02:40 +0530

    Time: 181 ms (77.09%) | Memory: 51.4 MB (65.82%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -1,23 +1,38 @@
 class Solution {
+bool checkSubseq(char c,vector<vector<int>> &alpha_idx,int &last_idx){
+    auto search_idx = upper_bound(alpha_idx[c-'a'].begin(),alpha_idx[c-'a'].end(),last_idx);
+    if(search_idx == alpha_idx[c-'a'].end()){
+        return false;
+    }
+    last_idx = *search_idx;
+    return true;
+    // for(auto search_idx : alpha_idx[c-'a']){
+    //     int idx = (int) search_idx;
+    //     if(idx > last_idx){
+    //         last_idx = idx;
+    //         return true;
+    //     }
+    // }
+    // return false;
+}
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        vector<vector<int>> alpha (26);
-		for (int i = 0; i < s.size (); ++i) alpha[s[i] - 'a'].push_back (i);
-		int res = 0;
+        vector<vector<int>> alpha_idx(26);
+        int count = 0;
+        for(int i = 0 ;i < s.size() ; i++) 
+        alpha_idx[s[i]-'a'].push_back(i);
 
-		for (auto &word : words) {
-			int x = -1;
-			bool found = true;
+        for(auto &word : words){
+            int last_idx = -1;
+            int char_count = 0;
+            for(auto c : word){
+                if(checkSubseq(c,alpha_idx,last_idx)) char_count++;
+                else break;
+            }
+            if(char_count==word.size()) count++;
+        }
+        
+        return count;
 
-			for (char c : word) {
-				auto it = upper_bound (alpha[c - 'a'].begin (), alpha[c - 'a'].end (), x);
-				if (it == alpha[c - 'a'].end ()){ found = false;  break; }
-				else x = *it;
-			}
-
-			if (found) res++;
-		}
-
-		return res;
     }
 };
