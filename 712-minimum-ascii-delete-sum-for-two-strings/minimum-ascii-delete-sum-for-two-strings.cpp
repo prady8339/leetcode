cc6b4e21d110324d8e8a09be4b325eca132cc239
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-24 01:55:46 +0530

    Time: 29 ms (77.33%) | Memory: 15.3 MB (37.46%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    long long help(int i,int j,string& s1,string &s2,vector<vector<int>>&dp){
-        if(i==s1.size() && j==s2.size())return 0;
-        if(i==s1.size()){
-            int d=0;
-            for(int k=j;k<s2.size();k++){
-                d += (int)s2[k];
-            }
-            return d;
-        }
-        if(j==s2.size()){
-            int d=0;
-            for(int k=i;k<s1.size();k++){
-                d += (int)s1[k];
-            }
-            return d;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        long long ans = INT_MAX;
-        if(s1[i]==s2[j])ans = help(i+1,j+1,s1,s2,dp);
-        else {
-            ans = min(ans, s1[i] + help(i+1,j,s1,s2,dp));
-            ans = min(ans, s2[j] + help(i,j+1,s1,s2,dp));
-        }
-        return dp[i][j] = ans;
-    }
     int minimumDeleteSum(string s1, string s2) {
-        vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
-        return help(0,0,s1,s2,dp);
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+
+        int sum1 = 0;
+        for(int i = n ; i >= 0 ; i--){
+            dp[i][m] = sum1;
+            if(i>0)
+            sum1+=s1[i-1];
+        }
+        int sum2 = 0;
+        for(int j = m ; j >= 0 ; j--){
+            dp[n][j] = sum2;
+            if(j>0)
+            sum2+=s2[j-1];
+        }
+        dp[n][m]=0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    int del1 = s1[i] + dp[i + 1][j]; // Update with ASCII value
+                    int del2 = s2[j] + dp[i][j + 1]; // Update with ASCII value
+                    dp[i][j] = min(del1, del2);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
