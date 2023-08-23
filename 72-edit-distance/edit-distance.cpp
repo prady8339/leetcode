57ea8780a1fc80fec3c05b7af3955f7d8c98ba5f
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-24 00:08:39 +0530

    Time: 15 ms (52.74%) | Memory: 9.1 MB (26.87%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -9,29 +9,40 @@ public:
 
        for(int i = 0 ; i < n+1 ; i++){
            //size == max operation
-           dp[i][0] = i;
+           dp[i][m] = n-i;
        }
         for(int j = 0 ; j < m+1 ; j++){
            //size == max operation
-           dp[0][j] = j;
+           dp[n][j] = m-j;
        }
     
     // we word1[i]==word2[j] we just simply move further with no cost
     // else we choose minmum from 3 cases either replace , insert ,delete
 
-       for(int i = 1 ;i < n+1 ; i++){
-           for(int j = 1 ; j < m+1 ; j++){
-               if(word1[i-1] == word2[j-1]){
-                   dp[i][j] = dp[i-1][j-1];
-               }else{
-                   // replace , delete ,insert
-                   dp[i][j] = min({dp[i-1][j-1]+1,dp[i][j-1]+1,dp[i-1][j]+1});
-                   // for better intution itterate dp in reverse and same with initilization 
-                   // then return dp[0][0] for for now we return dp[n][m]
-               }
-           }
-       }
+    //    for(int i = 1 ;i < n+1 ; i++){
+    //        for(int j = 1 ; j < m+1 ; j++){
+    //            if(word1[i-1] == word2[j-1]){
+    //                dp[i][j] = dp[i-1][j-1];
+    //            }else{
+    //                // replace , delete ,insert
+    //                dp[i][j] = min({dp[i-1][j-1]+1,dp[i][j-1]+1,dp[i-1][j]+1});
+    //                // for better intution itterate dp in reverse and same with initilization 
+    //                // then return dp[0][0] 
+    //            }
+    //        }
+    //    }
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int j = m-1 ; j >= 0 ; j--){
+                if(word1[i] == word2[j]){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = 1 + min({ dp[i+1][j+1], dp[i+1][j], dp[i][j+1] });
+                }
+            }
+        }
 
-       return  dp[n][m] ; 
+        return dp[0][0];
+      // return  dp[n][m] ; 
     }
 };
