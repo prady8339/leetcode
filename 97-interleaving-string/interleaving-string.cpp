46fedc5b06ca326094d3ab89d242fd2df29c1562
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-08-25 07:10:49 +0530

    Time: 4 ms (70.98%) | Memory: 12.3 MB (41.03%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    bool solve(string &s1, string &s2, string &s3, int n1, int n2, int n3, unordered_map<int, int> &dp) {
+        if (n3 < 0) {
+            return true;
+        }
+        
+        int key = n1 * 10000 + n2 * 100 + n3;  // Create a unique key for memoization
+        
+        if (dp.find(key) != dp.end()) {
+            return dp[key];
+        }
+        
+        bool c1 = false, c2 = false;
+
+        if (n1 >= 0 && s3[n3] == s1[n1]) {
+            c1 = solve(s1, s2, s3, n1 - 1, n2, n3 - 1, dp);
+        }
+        if (n2 >= 0 && s3[n3] == s2[n2]) {
+            c2 = solve(s1, s2, s3, n1, n2 - 1, n3 - 1, dp);
+        }
+
+        return dp[key] = c1 || c2;
+    }
+
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        int n3 = s3.size();
+
+        if (n1 + n2 != n3) {
+            return false;
+        }
+
+        unordered_map<int, int> dp;
+        
+        return solve(s1, s2, s3, n1 - 1, n2 - 1, n3 - 1, dp);
+    }
+};
