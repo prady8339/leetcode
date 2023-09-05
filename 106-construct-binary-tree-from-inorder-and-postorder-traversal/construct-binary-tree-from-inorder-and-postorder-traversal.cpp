cf5fc9d4e1a2dbfbe61991d12d1fd67ebc0cdd66
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-09-05 21:58:12 +0530

    Time: 22 ms (41.39%) | Memory: 26.1 MB (73.72%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,43 +11,43 @@
  */
 class Solution {
 private:
-    // TreeNode* helper(vector<int>& inorder,vector<int> &postorder,int l,int r,int &idx){
-    //     if( l > r || idx>=inorder.size()) return NULL;
+    TreeNode* helper(vector<int>& inorder,vector<int> &postorder,int l,int r,int &idx){
+        if( l > r || idx>=inorder.size()) return NULL;
 
-    //     int root = postorder[idx];
-    //     //find root in inorder;
-    //     int pi = l;
-    //     while(inorder[pi] != root) pi++;
+        int root = postorder[idx];
+        //find root in inorder;
+        int pi = l;
+        while(inorder[pi] != root) pi++;
 
-    //     // move to next root;
-    //     idx--;
+        // move to next root;
+        idx--;
 
-    //     TreeNode* node = new TreeNode(root);
+        TreeNode* node = new TreeNode(root);
 
-    //     node->left  = helper(inorder,postorder,pi - 1, r ,idx);
-    //     node->right = helper(inorder,postorder,l,pi + 1,idx);
+        node->right = helper(inorder,postorder,pi+1,r,idx);
+        node->left  = helper(inorder,postorder,l,pi-1,idx);
 
-    //     return node;
-    // }
+        return node;
+    }
 
-TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int l, int r, int& idx) {
-    if (l > r) return NULL;
+// TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int l, int r, int& idx) {
+//     if (l > r) return NULL;
 
-    int root = postorder[idx];
-    // Find root in inorder;
-    int pi = l;
-    while (inorder[pi] != root) pi++;
+//     int root = postorder[idx];
+//     // Find root in inorder;
+//     int pi = l;
+//     while (inorder[pi] != root) pi++;
 
-    // Decrement idx before calling for the right subtree.
-    idx--;
+//     // Decrement idx before calling for the right subtree.
+//     idx--;
 
-    TreeNode* node = new TreeNode(root);
+//     TreeNode* node = new TreeNode(root);
 
-    node->right = helper(inorder, postorder, pi + 1, r, idx); // Corrected this line.
-    node->left = helper(inorder, postorder, l, pi - 1, idx);
+//     node->right = helper(inorder, postorder, pi + 1, r, idx); // Corrected this line.
+//     node->left = helper(inorder, postorder, l, pi - 1, idx);
 
-    return node;
-}
+//     return node;
+// }
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = inorder.size();
