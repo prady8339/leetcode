e8877f9e377db19e47101feb6f3adfa7050d4b37
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-09-05 22:18:30 +0530

    Time: 28 ms (28.57%) | Memory: 25.9 MB (84.29%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -30,24 +30,6 @@ private:
         return node;
     }
 
-// TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int l, int r, int& idx) {
-//     if (l > r) return NULL;
-
-//     int root = postorder[idx];
-//     // Find root in inorder;
-//     int pi = l;
-//     while (inorder[pi] != root) pi++;
-
-//     // Decrement idx before calling for the right subtree.
-//     idx--;
-
-//     TreeNode* node = new TreeNode(root);
-
-//     node->right = helper(inorder, postorder, pi + 1, r, idx); // Corrected this line.
-//     node->left = helper(inorder, postorder, l, pi - 1, idx);
-
-//     return node;
-// }
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = inorder.size();
