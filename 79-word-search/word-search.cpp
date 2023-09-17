8f3ae280caccd06531967572511d03af0721ff3f
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-09-17 18:19:08 +0530

    Time: 635 ms (50.06%) | Memory: 8.5 MB (7.33%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,9 +1,9 @@
 class Solution {
+    int n ;
+    int m ;
 private:
     vector<vector<int>> dirs{{1,0},{-1,0},{0,1},{0,-1}};
     bool check(vector<vector<char>>& board, string &word,int idx,int i,int j){
-        int n = board.size();
-        int m = board[0].size();
         if(idx>=word.size()) return true;
         if(i >= n || i<0 || j >= m || j < 0) return false;
         if(board[i][j] != word[idx]) return false;
@@ -14,14 +14,15 @@ private:
             char preserve = board[i][j];
             board[i][j] = '#';
             ans = ans | check(board,word,idx+1,i+dir[0],j+dir[1]);
+            if(ans) break;
             board[i][j] = preserve;
         }
         return ans;
     }
 public: 
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
+        n = board.size();
+        m = board[0].size();
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
