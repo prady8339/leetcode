db6da0397601e6d688fddf9ddbff55df7e3483d0
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-09-24 01:08:51 +0530

    Time: 47 ms (98.50%) | Memory: 21.2 MB (43.41%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+bool isSubseq(string &a,string &b){
+    //a is small 
+    int l = 0 , r = 0;
+    while(l < a.size() && r < b.size()){
+        if(a[l]==b[r]){
+            l++;
+        }
+        r++;
+    }
+    return l==a.size();
+}
+int solve(unordered_map<int,vector<string>> &ds,unordered_map<string,int> &dp,int sz,string word){
+    if(sz<=0) return 0;
+    int res = 0;
+    if(dp.count(word)) return dp[word];
+    if(sz-1>=0){
+        for(auto &a:ds[sz-1]){
+            if(isSubseq(a,word)){
+                res = max(res,solve(ds,dp,sz-1,a));
+            }
+        }
+    }
+    return dp[word] = res+1;
+}
+public:
+    int longestStrChain(vector<string>& words) {
+        unordered_map<int,vector<string>> ds;
+  
+        for(int i = 0 ; i < words.size() ; i++){
+            ds[words[i].size()].push_back(words[i]);
+        }
+        
+        unordered_map<string,int> dp;
+        int maxi = 0;
+        for(auto &grp:ds){
+            int sz = grp.first;
+            for(auto &word:grp.second){
+                int temp = solve(ds,dp,sz,word);
+                maxi = max(temp,maxi);
+            }
+        }
+        return maxi;
+    }
+};
+
