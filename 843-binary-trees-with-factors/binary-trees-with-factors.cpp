5a675d4830c6947ab0f714bbe6409350628be1a5
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-10-27 22:25:34 +0530

    Time: 142 ms (9.45%) | Memory: 34 MB (5.12%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.cpp
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -1,31 +1,33 @@
-const int MOD = 1e9 + 7;
-
 class Solution {
 public:
-    int numFactoredBinaryTrees(std::vector<int>& arr) {
-        std::sort(arr.begin(), arr.end());
-        std::unordered_set<int> s(arr.begin(), arr.end());
-        std::unordered_map<int, int> dp;
-        for (int x : arr) dp[x] = 1;
-        
-        for (int i : arr) {
-            for (int j : arr) {
-                if (j > std::sqrt(i)) break;
-                if (i % j == 0 && s.find(i / j) != s.end()) {
-                    long long temp = (long long)dp[j] * dp[i / j];
-                    if (i / j == j) {
-                        dp[i] = (dp[i] + temp) % MOD;
-                    } else {
-                        dp[i] = (dp[i] + temp * 2) % MOD;
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        unordered_map<int,int> dp;
+        int mod = 1e9+7;
+        int n = arr.size();
+        for(auto &x:arr){
+            dp[x]=1;
+        }
+        for(int i = 0 ; i < n ; i++){
+            int curr = arr[i];
+            for(int j = 0 ; 1 ; j++){
+                int prevs = arr[j];
+                if(prevs > sqrt(curr)) break;
+                long long temp = (long long) dp[prevs] * dp[curr / prevs];
+                if(curr % prevs == 0 && dp.count(prevs)){
+                    if(curr/prevs==prevs){
+                        dp[curr] = (dp[curr] + temp) % mod;
+                    }else{
+                        dp[curr] = (dp[curr] + temp * 2 ) % mod;
                     }
                 }
             }
         }
-        
-        int result = 0;
-        for (auto& [_, val] : dp) {
-            result = (result + val) % MOD;
+        int res = 0;
+        for(auto &[_,val]:dp){
+            res = (res + val) % mod;
         }
-        return result;
+
+        return res;
     }
 };
