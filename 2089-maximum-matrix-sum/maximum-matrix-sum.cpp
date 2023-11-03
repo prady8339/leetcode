c1dcfd9731152a025f652640d40bf9f029612012
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-11-03 12:53:23 +0530

    Time: 133 ms (18.33%) | Memory: 35.6 MB (54.17%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
+
         int pos = 0, neg = 0, zer = 0;
         long long sum = 0;
         int mini = INT_MAX;
+
         for(int i = 0 ; i < matrix.size() ; i++){
             for(int j = 0 ; j < matrix[i].size() ; j++){
-                mini = min(abs(matrix[i][j]),mini);
-                sum += abs(matrix[i][j]);
-                if(matrix[i][j]>0) pos++;
-                else if(matrix[i][j]<0) neg++;
+                mini = min(abs( matrix[i][j]), mini);
+                sum += abs( matrix[i][j] );
+                if( matrix[i][j] > 0 ) pos++;
+                else if( matrix[i][j] < 0 ) neg++;
                 else zer++;
             }
         }
-        // cout<<mini;
+
         if(zer || neg%2 == 0) return sum;
         else return sum - 2*mini;
     }
