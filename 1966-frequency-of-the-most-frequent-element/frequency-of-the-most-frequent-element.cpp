86cb09ae383eff189669159b12ff5a118dc90fdf
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-11-18 11:11:07 +0530

    Time: 804 ms (5.15%) | Memory: 99.3 MB (85.20%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,27 +1,30 @@
 class Solution {
-unordered_map<int,int> freq;
+
 int solve(vector<int>& nums, int k,int idx){
     int x = nums[idx];
     int ct = 1;
-    if(freq.count(x)) return freq[x];
     for(int i = idx-1 ; i >= 0 ; i--){
         if(abs(nums[i]-x) <= k){
             ct++;
             k-=abs(nums[i]-x);
         }else break;
     }
-    return freq[x] = ct;
+    return ct;
 }
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
         int maxi = 0;
         int n = nums.size();
+        int prev = INT_MAX;
         for(int i = n-1 ; i >= 0 ; i--){
-            int res = solve(nums,k,i);
-            if(res > maxi){
-                maxi = res;
+            if(prev != nums[i]){
+                int res = solve(nums,k,i);
+                if(res > maxi){
+                    maxi = res;
+                }
             }
+            prev = nums[i];
         }
         return maxi;
     }
