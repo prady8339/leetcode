d5b53b938f98a423925642c718f8f42ebac7a20e
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-12-02 16:52:06 +0530

    Time: 171 ms (90.72%) | Memory: 126.6 MB (69.96%) - LeetSync

--- 3166-minimum-number-of-groups-to-create-a-valid-assignment/minimum-number-of-groups-to-create-a-valid-assignment.cpp
+++ 3166-minimum-number-of-groups-to-create-a-valid-assignment/minimum-number-of-groups-to-create-a-valid-assignment.cpp
@@ -1,29 +1,32 @@
 class Solution {
-int minGrps(int &x,unordered_map<int,int> &freq){
-    int ans = 0;
-    if(x <= 0) return INT_MAX;
-    for(auto &[_, val]: freq){
-        int remainder = val % x;
-        int times = val / x;
-        if(remainder > times) return INT_MAX;
-        ans += ceil((float) val / (x + 1));
+    int minGrps(int &grpSz, unordered_map<int, int> &freq) {
+        int ans = 0;
+        if (grpSz <= 0) return INT_MAX;
+        
+        for (auto &[_, val] : freq) {
+            int remainder = val % grpSz;
+            int times = val / grpSz;
+            if (remainder > times) return INT_MAX;
+            ans += ceil((float)val / (grpSz + 1));
+        }
+        
+        return ans;
     }
-    return ans;
-}
+
 public:
-    int minGroupsForValidAssignment(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        for(auto &num : nums){
-            freq[num]++; 
-        }
+    int minGroupsForValidAssignment(vector<int> &nums) {
+        unordered_map<int, int> freq;
         int smallestGrp = INT_MAX;
-        for(auto &[key, value]: freq){
-            smallestGrp = min(smallestGrp, value);
-        }
         int ans = INT_MAX;
-        for(int i = 1 ; i <= smallestGrp ; i++){
-            ans = min(ans, minGrps(i,freq));
+        
+        for (auto &num : nums) freq[num]++;
+        
+        for (auto &[key, value] : freq) smallestGrp = min(smallestGrp, value);
+        
+        for (int grpSz = 1; grpSz <= smallestGrp; grpSz++) {
+            ans = min(ans, minGrps(grpSz, freq));
         }
+        
         return ans;
     }
 };
