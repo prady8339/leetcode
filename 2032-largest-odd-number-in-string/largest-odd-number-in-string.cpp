3bc0e329806660e36a9beb8a5897b8f11afcf1b7
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-12-07 19:33:49 +0530

    Time: 19 ms (98.43%) | Memory: 15.3 MB (58.43%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,15 +4,13 @@ public:
         string ans;
         bool f = 0;
         int n = num.size();
-        for(int i = n-1 ; i>= 0 ; i--){
+        int i = n-1;
+        for( ; i>= 0 ; i--){
             if( (int)(num[i]-'0') % 2 != 0){
-                f = 1;
+                break;
             }
-            if(f){
-                ans.push_back(num[i]);
-            }
+            
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return num.substr(0,i+1);
     }
 };
