e5ff3581c0dfd3cc22a21951f3add1e2a603740d
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-12-15 22:55:03 +0530

    Time: 830 ms (10.55%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,1 @@
-SELECT name FROM Employee WHERE id IN (SELECT managerId FROM Employee GROUP BY managerId having count(*)>=5 ) ;
-
-# SELECT managerId FROM Employee GROUP BY managerId having count(*)>=5
+select e2.name from Employee e1 inner join Employee  e2 on e1.managerId = e2.id group by e2.id having count(e2.id) >= 5;
