53bef790fb107fdf24b29574ffbd080c16761798
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2023-12-25 15:07:34 +0530

    Time: 4 ms (23.71%) | Memory: 6.8 MB (29.05%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,42 +1,27 @@
 class Solution {
-
-bool valid(string c){
-    int len = c.size();
-    if(len > 2) return false;
-    if((len == 2 || len == 1) && c[0]=='0') return false;
-    int t = stoi(c);
-    if(t > 0 && t <= 26) return true;
-    return false;
-}
-
-int solve(string &s,int i,vector<int> &dp){
-    if(i == s.size()){
-        return 1;
-    }
-
-    if(dp[i]!= -1) return dp[i];
-    string a = s.substr(i,1);
-    string b = s.substr(i,2);
-
-    int oneStep = 0, twoStep = 0;
-
-    if(valid(a)){
-        oneStep +=  solve(s, i+1, dp);
-    }else{
-        // cuz not possible anyhow
-        return 0;
-    }
-
-    if(valid(b) && i + 1 < s.size()){
-        twoStep +=  solve(s, i+2, dp);
-    }
-    //no else cuz it can be possible using oneStep
-
-    return dp[i] = oneStep + twoStep;
-}
 public:
     int numDecodings(string s) {
-    vector<int> dp(s.length(), -1);
-       return solve(s,0,dp);
+        int n = s.size();
+
+        if (s.empty() || s[0] == '0') return 0;
+        if (n == 1) return 1;
+        
+        vector<int> dp(n+1 , 0);
+        dp[0] = 1;
+        dp[1] = 1;
+          
+        for (int i = 2; i <= n; ++i) {
+            int oneDigit = stoi(s.substr(i - 1, 1));
+            int twoDigits = stoi(s.substr(i - 2, 2));
+            
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
     }
 };
