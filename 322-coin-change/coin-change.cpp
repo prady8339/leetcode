faf440b2c694f6e348e3887864c53654c8fa932f
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-01-17 19:36:26 +0530

    Time: 61 ms (75.76%) | Memory: 14.9 MB (63.07%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amt) {
-        int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int> (amt+1));
-  
-        for(int j = 0 ; j < amt+1 ; j++){
-            dp[0][j]=1e5;
-        }
-
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1; j < amt+1 ; j++){
-                if(j>=coins[i-1]){
-                    dp[i][j] = min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
-                }else{
-                    dp[i][j]=dp[i-1][j];
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, 1e9);
+        dp[0] = 0;
+        for(int i = 1 ; i <= amount ; i++){
+            for(auto &c : coins){
+                if(i - c >= 0){
+                    dp[i] = min(dp[i], 1 + dp[i - c]);
                 }
             }
         }
-        return dp[n][amt]==1e5?-1:dp[n][amt];
+        return dp[amount] == 1e9 ? -1 : dp[amount];
     }
 };
