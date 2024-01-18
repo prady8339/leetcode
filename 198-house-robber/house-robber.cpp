dc73a14fc5f53870c48b863a431cbedd2d50b5a7
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-01-18 08:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (85.96%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int r1 = 0, r2 = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            int temp = max(nums[i] + r2 , r1);
+            r2 = r1;
+            r1 = temp;
+        }
+        return r1;
+    }
+};
