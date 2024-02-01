29f4351e249ff1ca46db8c2b3c8bb85f388a83e0
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-02-01 11:42:16 +0530

    Time: 784 ms (5.09%) | Memory: 411.9 MB (7.82%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,9 +1,41 @@
 class Solution {
+private:
+void merge(vector<int> &arr, int start, int mid, int end) {
+    vector<int> temp;
+    int l = start;
+    int r = mid + 1;
+    while(l <= mid && r <= end){
+        if(arr[l] < arr[r]){
+            temp.push_back(arr[l++]);
+        }else{
+            temp.push_back(arr[r++]);
+        }
+    }
+    while(l <= mid){
+        temp.push_back(arr[l++]);
+    }
+    while(r <= end){
+        temp.push_back(arr[r++]);
+    }
+    for(int i = 0 ; i < temp.size() ; i++){
+        arr[start + i] = temp[i];
+    }
+    return ;
+}
+
+void mergeSort(vector<int> &arr, int start,int end){
+    if(start < end){
+        int mid = start + (end - start) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
+    }
+}
+
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
         int n = nums.size();
-        // mergeSort(nums,0, n - 1);
-        sort(nums.begin(), nums.end());
+        mergeSort(nums,0, n - 1 );
         vector<vector<int>> ans;
         vector<int> v;
         for(int i = 0 ; i < n ; i++){
