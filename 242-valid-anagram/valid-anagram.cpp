04aafbd224f88a2efeb3424d29abf13fa989940f
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-02-05 09:22:41 +0530

    Time: 7 ms (74.17%) | Memory: 8.5 MB (11.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s == t;
+        vector<int> v(26,0);
+        if(s.size() != t.size()) return false;
+        for(int i = 0 ; i < t.size() ; i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(auto &i: v) if(i != 0) return false;
+        return true;
     }
 };
