5e62a4692acdc914cfb711461a07efab369588fe
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-02-18 09:31:56 +0530

    Time: 70 ms (85.67%) | Memory: 57.5 MB (21.99%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        priority_queue<int, vector<int>, greater<int>> jumps;
-
-        for (int i = 0; i < n - 1; i++) {
-            int diff = heights[i + 1] - heights[i];
-            if (diff > 0) {
-                jumps.push(diff);
-
-                if (jumps.size() > ladders) {
-                    int bricks_needed = jumps.top();
-                    jumps.pop();
-
-                    if (bricks_needed > bricks) return i;
-                    else bricks -= bricks_needed;
+        priority_queue<int> pq;
+        for(int i = 0 ; i < heights.size() - 1 ; i++){
+            int diff = heights[i + 1] -  heights[i];
+            if(diff > 0){
+                pq.push(diff);
+                if(bricks >= diff){
+                    bricks -= diff;
+                }else if(ladders > 0){
+                    bricks += pq.top();
+                    bricks -= diff;
+                    ladders -= 1;
+                    pq.pop();
+                }else{
+                    return i;
                 }
             }
         }
-        return n - 1;
+        return heights.size() - 1;
     }
 };
