1b5fb25c2ffc643e697c108287df916192db3ed5
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-03-20 07:26:49 +0530

    Time: 77 ms (46.61%) | Memory: 67.5 MB (5.93%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,18 +1,33 @@
 class Solution {
+private:
+    void reduce(priority_queue<pair<int,char>> &pq, int k,int &ans){
+        stack<pair<int,int>> st;
+        while(!pq.empty() && k > 0){
+            k--;
+            ans++;
+            auto tp = pq.top();
+            if(tp.first - 1  > 0)
+            st.push({tp.first - 1, tp.second});
+            pq.pop();
+        }
+        if(k > 0 && !st.empty()){
+            ans+= k;
+        }
+        while(!st.empty()){
+            pq.push(st.top());
+            st.pop();
+        }
+    }
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int freq[26] = {0};
-        for(char task : tasks){
-            freq[task - 'A']++;
+        priority_queue<pair<int,char>> pq;
+        unordered_map<char,int> freq;
+        for(auto &t : tasks) freq[t]++;
+        for(auto &[k, v]: freq) pq.push({v,k});
+        int ans = 0;
+        while(!pq.empty()){
+            reduce(pq, n + 1,ans);
         }
-        sort(begin(freq) , end(freq));
-        int chunk = freq[25] - 1;
-        int idel = chunk * n;
-
-        for(int i=24; i>=0; i--){
-            idel -= min(chunk,freq[i]);
-        }
-
-        return idel < 0 ? tasks.size() : tasks.size() + idel;
+        return ans;
     }
 };
