56e9105d4936f77c4f65c7268b88ac94cd1f5e4c
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-04-26 22:58:23 +0530

    Time: 395 ms (14.43%) | Memory: 18.3 MB (34.89%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int getAns(vector<vector<int>>&grid,int n,int prev,vector<vector<int>>&dp){
-        // if(n==0){
-        //     int mini=1e8;
-        //     for(int i=0;i<grid.size();i++){
-        //             if(prev == i) continue;
-        //             mini=min(mini,grid[n][i]);
-        //     }
-        //     return mini;
-        // }
-        if(n < 0) return 0;
-        if(dp[n][prev]!=-1)return dp[n][prev];
+    int solve(vector<vector<int>>&grid,int i,int pc,vector<vector<int>>&dp){
+        if(i >= grid.size()) return 0;
+        if(dp[i][pc]!=-1)return dp[i][pc];
         int mini=1e8;
         for(int j=0;j<grid.size();j++){
-            if(j == prev) continue;
-
-            int a =grid[n][j] +getAns(grid,n-1,j,dp);
+            if(j == pc) continue;
+            int a = grid[i][j] + solve(grid,i+1,j,dp);
             mini=min(mini,a);
    
         }
-        return dp[n][prev] = mini;
+        return dp[i][pc] = mini;
     }
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return getAns(grid,n-1,n,dp);
+        return solve(grid,0,n,dp);
     }
 };
