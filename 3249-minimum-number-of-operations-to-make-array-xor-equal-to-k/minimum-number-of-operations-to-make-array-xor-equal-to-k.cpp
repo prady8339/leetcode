556217a77e8973e744b6f296b82fc2ac3eaf032c
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-04-29 22:40:09 +0530

    Time: 114 ms (44.50%) | Memory: 91.4 MB (25.13%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -2,9 +2,8 @@ class Solution {
 
 public:
     int minOperations(vector<int>& nums, int k) {
-        int sumXor = 0;
+        int sumXor = k;
         for(auto &x : nums) sumXor ^= x;
-        sumXor ^= k;
         return __builtin_popcount(sumXor);;
     }
 };
