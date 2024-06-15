e8d57a052720d496eda172d048701f8a242e6489
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-06-15 23:36:38 +0530

    Time: 11 ms (90.62%) | Memory: 21.6 MB (38.74%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l = 0;
+        for(int i = 0; i < nums.size() ; i++){
+            if(nums[i] != 0){
+                nums[l] = nums[i];
+                l++;
+            }
+        }
+        while(l < nums.size()){
+            nums[l] = 0;
+            l++;
+        }
+    }
+};
