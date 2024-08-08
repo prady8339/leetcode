2bf81aeec3c47f6034f7511ac551cc5b935becbb
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-08-09 02:14:34 +0530

    Time: 7 ms (86.72%) | Memory: 13.8 MB (25.56%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.cpp
+++ 921-spiral-matrix-iii/spiral-matrix-iii.cpp
@@ -1,28 +1,23 @@
 class Solution {
-
-private:
-    bool valid(int row,int col,int r,int c){
-        if(r < 0 || r >= row || c < 0 || c >= col) return false;
-        return true;
-    }
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int r, int c) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        auto valid = [&](int &row,int &col,int &r,int &c) -> bool {
+            return !(r < 0 || r >= row || c < 0 || c >= col);
+        };
+
         int total = rows * cols;
-
         vector<vector<int>> ans;
-
         int x = 0;
         int y = 1;
-
-        int r = rStart;
-        int c = cStart;
-
-        int aukat = 1;
+        int a = 1;
         int round = 0;
 
-        while(ans.size() != total){
-            for(int i = 0 ; i < aukat ; i++){
-                if(valid(rows,cols,r,c)) ans.push_back({r,c});
+        while(total){
+            for(int i = 0 ; i < a ; i++){
+                if(valid(rows,cols,r,c)){ ans.push_back({r,c}); total--; }
                 r+=x; c+=y;
             }
             if(x == 1){ x = 0; y = -1; }
@@ -30,14 +25,9 @@ public:
             else if(x == -1){ y = 1; x = 0; }
             else if(y == 1) { y = 0; x = 1; }
             round++;
-            if(round % 2 == 0) aukat++;
+            if(round % 2 == 0) a++;
         }
+
         return ans;
     }
 };
-
-
-// x+
-// y-
-// x-
-// y+
