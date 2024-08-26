8ac392e65a36899d1509652162237df2403cadea
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-08-26 14:09:34 +0530

    Time: 32 ms (69.98%) | Memory: 25.4 MB (20.06%) - LeetSync

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    public:
-    std::vector<std::vector<int>> imageSmoother(std::vector<std::vector<int>>& img) {
-        int m = img.size();
-        int n = img[0].size();
-        std::vector<std::vector<int>> res(m, std::vector<int>(n, 0));
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int n = img.size(), m = img[0].size();
+        vector<vector<int>> res(n, vector<int>(m, 0));
+        vector<vector<int>> dirs{
+            {1, 1},  {-1, -1}, {1, 0},  {0, 1}, {-1, 0},
+            {0, -1}, {-1, 1},  {1, -1}, {0, 0},
+        };
+        auto is_valid = [&](int i, int j) -> bool {
+            return (i >= 0 && i < n && j >= 0 && j < m);
+        };
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                res[i][j] = smoothen(img, i, j);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int sum = 0;
+                int e = 0;
+
+                for (auto& d : dirs) {
+                    int x = d[0] + i;
+                    int y = d[1] + j;
+                    if (is_valid(x, y)) {
+                        e++;
+                        sum += img[x][y];
+                    }
+                }
+                res[i][j] = (int)(sum / e);
             }
         }
 
         return res;
-    }
-
-private:
-    int smoothen(const std::vector<std::vector<int>>& img, int x, int y) {
-        int m = img.size();
-        int n = img[0].size();
-        int sum = 0;
-        int count = 0;
-
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
-                int nx = x + i;
-                int ny = y + j;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    sum += img[nx][ny];
-                    ++count;
-                }
-            }
-        }
-
-        return sum / count;
     }
 };
