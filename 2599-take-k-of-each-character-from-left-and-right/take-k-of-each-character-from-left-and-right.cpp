ed2b40abe4b3157ca5a2a46e64a626b09bf7cf78
Pradyumna Singh <76277136+prady8339@users.noreply.github.com>
2024-11-21 00:31:40 +0530

    Time: 96 ms (16.59%) | Memory: 13.1 MB (33.23%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -17,16 +17,16 @@ public:
 
         int n = s.size();
         int l = 0, r = 0;
-        int ans = INT_MAX;
+        int ans = 0;
         while (r < n) {
             mp[s[r]]--;
             while (!is_valid()) {
                 mp[s[l]]++;
                 l++;
             }
-            ans = min(ans,n - (r - l + 1));
+            ans = max(ans,(r - l + 1));
             r++;
         }
-        return ans;
+        return n - ans;
     }
 };
